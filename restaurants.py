483e30cdac210469cb0e7c829364f33812096368
Jed Woods <134558375+jedwoods@users.noreply.github.com>
2023-10-14 11:48:39 -0600

    Update restaurants.py
    Added comments for readability

--- restaurants.py
+++ restaurants.py
@@ -7,35 +7,39 @@ import time
 import pandas as pd
 
 
-
+# get the API Key
 with open('secrets.json') as infile:
     keys = json.load(infile)
     gkey = keys['gkey']
 
+# Open the google client using our key and initialize an empty datafram
 gmaps = googlemaps.Client(key = gkey)
-
 df = pd.DataFrame()
 
-
+# get the places around our location, google will give up to 20 different locales
 places = gmaps.places_nearby(location = '39.790876, -86.142675', radius = '40000', keyword = 'restaurant')
-for place in places['results']:
-    my_place_id = place['place_id']
 
+# Sort through the locales and send in a call to get specific fields
+for place in places['results']:
+    # get current ID
+    my_place_id = place['place_id']
+    
+    # define the fields we want data for
     my_fields = ['name', 'type',  'takeout', 'adr_address', 'price_level', 'rating', 'serves_breakfast', 'serves_dinner']
 
+    # make the call to get the locale data
     my_place = gmaps.place(place_id = my_place_id, fields = my_fields)
-
-    result = my_place_id
+    
+    # save the data to our DataFrame
     temp = pd.DataFrame.from_dict(my_place['result'])
     df = pd.concat([df,temp])
 
 
-
+# pause the script so Google's server can catch up
 time.sleep(3) 
 
-
+# repeat the steps above to get more data
 places = gmaps.places_nearby(page_token = places['next_page_token'])
-
 for place in places['results']:
     my_place_id = place['place_id']
 
@@ -44,6 +48,7 @@ for place in places['results']:
     my_place = gmaps.place(place_id = my_place_id, fields = my_fields)
     temp = pd.DataFrame.from_dict(my_place['result'])
     df = pd.concat([df,temp])
+
 
 time.sleep(3)
 
@@ -60,6 +65,6 @@ for place in places['results']:
     df = pd.concat([df,temp])
 
 
-# outfile = open('data.txt', 'w')
+# save the data to a csv
 outfile = 'data.csv'
 df.to_csv(outfile)
